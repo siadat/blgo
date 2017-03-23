633de8a843f013a2908318afa77edc3f7aaa66cc
Mohamad Jahani <m4m4lj@gmail.com>
2017-03-23 18:00:00 +0430

    refactor(blgo): fix a variable name typo

--- blgo.go
+++ blgo.go
@@ -60,7 +60,7 @@ func parseFrontmatter(body *[]byte) (frontmatter map[interface{}]interface{}) {
 	frontmatter = make(map[interface{}]interface{})
 
 	buf := bytes.NewBuffer(*body)
-	stared, ended := false, false
+	started, ended := false, false
 	for {
 		line, err := buf.ReadString('\n')
 		if err != nil {
@@ -68,13 +68,13 @@ func parseFrontmatter(body *[]byte) (frontmatter map[interface{}]interface{}) {
 		}
 
 		if line == "---\n" {
-			if stared == false {
-				stared = true
+			if started == false {
+				started = true
 			} else if ended == false {
 				ended = true
 			}
 		}
-		if stared != false {
+		if started != false {
 			frontmatterBuf.Write([]byte(line))
 		}
 		if ended != false {
